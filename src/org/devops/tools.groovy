a5d6188538931bc9b1113e75b2451804b8a710c0
devops1987 <38104415+devops1987@users.noreply.github.com>
2020-04-04 23:54:02 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 //格式化日志输出字体颜色
-def PrintMes(value,color){
+def PrintMsg(value,color){
     colors = [
         'red': "\033[40;31m >>>>>>>>>>>> ${value} <<<<<<<<<<<< \033[0m",
         'blue': "\033[47;34m ++++++++++++ ${value} ++++++++++++ \033[0m",
